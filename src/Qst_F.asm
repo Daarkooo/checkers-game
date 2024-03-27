85546a7b560ce1f3c5212cdae2543506e4651d00
Medjber Abderrahim <medjber.abderrahim@gmail.com>
2024-03-27 17:08:04 +0100

    Update Qst_F.asm

--- src/Qst_F.asm
+++ src/Qst_F.asm
@@ -1,53 +1,31 @@
-.MODEL SMALL
+COMMENT @
+	EMPTY:			00h
 
-STACK SEGMENT PARA STACK
-	DB 64 DUP (0)
-STACK ENDS
+	BLACK: 	PAWN: 	'b'
+			QUEEN: 	02h (à changer)
 
-.DATA
-	myBoard DB 50 DUP (0)
+	WHITE: 	PAWN: 	'w'
+			QUEEN: 	04h (à changer)
+@
+board_init MACRO board
+	LOCAL L1, L2, L3
+	LEA SI, board
 
-.CODE
-;		EMPTY:			00h
+	MOV CX, 20
+	L1:
+		MOV BYTE PTR [SI], 'b'
+		INC SI
+	LOOP L1
+	
+	MOV CX, 10
+	L2:
+		MOV BYTE PTR [SI], 0
+		INC SI
+	LOOP L2
 
-;		BLACK: 	PAWN: 	'b'
-;				QUEEN: 	02h
-
-;		WHITE: 	PAWN: 	'w'
-;				QUEEN: 	04h
-	board_init MACRO
-		LEA SI, myBoard
-
-		MOV CX, 20
-		L1:
-			MOV BYTE PTR [SI], 'b'
-			INC SI
-		LOOP L1
-		
-		MOV CX, 10
-		L2:
-			MOV BYTE PTR [SI], 'w'
-			INC SI
-		LOOP L2
-
-		MOV CX, 20
-		L3:
-			MOV BYTE PTR [SI], 02h
-			INC SI
-		LOOP L3
-		RET
-	ENDM
-
-	main PROC FAR
-		
-        MOV AX, @DATA
-        MOV DS, AX
-		
-		board_init
-
-		MOV AH ,4Ch
-		INT 21h        
-        
-		RET
-	main ENDP
-END
+	MOV CX, 20
+	L3:
+		MOV BYTE PTR [SI], 'w'
+		INC SI
+	LOOP L3
+ENDM
