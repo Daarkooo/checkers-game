2e6590e7cbb87a9a05a5eef876eba1d73eef9ffb
Medjber Abderrahim <medjber.abderrahim@gmail.com>
2024-04-17 19:04:00 +0100

    Draws menu for GUI checkers

--- src/main/main.asm
+++ src/main/main.asm
@@ -3,25 +3,33 @@
 .STACK 100h
 
 .DATA
+    myBoard         DB  20 DUP('b'), 10 DUP('e'), 20 DUP('w')
+
+    SPMP_X          EQU     260
+    SPMP_Y          EQU     80
+    MP_MSG          DB      "Multi player$"
+    SP_MSG          DB      "Single player$"
+    isSP            DB      1
+
+    MAKLA_X         EQU     260
+    MAKLA_Y         EQU     150
+    MKLASIF_MSG     DB      "makla sif$"
+    MKLANSIF_MSG    DB      "machi sif$"
+    isMaklaSif      DB      1
+
+    START_X         EQU     260
+    START_Y         EQU     220
+    START_MSG       DB      "START$"
+
+    EXIT_X          EQU     5
+    EXIT_Y          EQU     312
+    EXIT_MSG        DB      "EXIT$"
 
     INCLUDE procs.inc
     INCLUDE GUI.inc
     INCLUDE print.inc
 
 .CODE
-    eventHandler PROC ; arg1, arg2, xPosition, yPosition
-        PUSH BP
-        MOV BP, SP
-
-        MOV DX, [BP + 4]
-        MOV CX, [BP + 6]
-
-        drawCircle 04h, CX, DX
-
-        MOV SP, BP
-        POP BP
-        RET 8
-    eventHandler ENDP
 
     main PROC
         MOV AX, @DATA
@@ -31,11 +39,17 @@
         MOV BP, SP
 
         ; set up video mode
-        MOV AX, 0012h
+        MOV AX, 0010h   ; 640x350 16 colors
         INT 10h
 
         ; set up mouse
         XOR AX, AX
+        INT 33h
+
+        ; set initial mouse position to (0, 0) to avoid distrubing the menu
+        MOV AX, 0004h
+        XOR CX, CX
+        XOR DX, DX
         INT 33h
 
         ; display mouse
@@ -52,13 +66,84 @@
         ; vertical range
         MOV AX, 0008h
         MOV CX, 0
-        MOV DX, 477
+        MOV DX, 347
         INT 33h
 
-        drawBoard 0, 0, 0Fh, 06h, 34
+        ; SP/MP TOGGLE
+        drawRectangle SPMP_X, SPMP_Y, 120, 50, 0006h
+        printGraphicalString SP_MSG, 0F1h, 34, 7
 
-        LEA AX, eventHandler
-        awaitMouseClick AX, 0, 0, 2
+        ; MAKLA TOGGLE
+        drawRectangle MAKLA_X, MAKLA_Y, 120, 50, 0002h
+        printGraphicalString MKLASIF_MSG, 0F2h, 35, 12
+
+        ; START BUTTON
+        drawRectangle START_X, START_Y, 120, 50, 0001h
+        printGraphicalString START_MSG, 0F6h, 37, 17
+
+        ; EXIT BUTTON
+        drawRectangle EXIT_X, EXIT_Y, 70, 30, 0004h
+        printGraphicalString EXIT_MSG, 0F4h, 3, 23
+
+        MAIN_L1:
+            LEA AX, getMenuOptionClicked
+            awaitMouseClick AX, 0, 0, 0
+
+            CMP AX, 0001h
+            JZ SPMP_clicked
+
+            CMP AX, 0002h
+            JNZ MAKLA_notClicked
+            JMP MAKLA_clicked
+            MAKLA_notClicked:
+
+            CMP AX, 0003h
+            JNZ START_notClicked
+            JMP START_clicked
+            START_notClicked:
+            
+            CMP AX, 000Fh
+            JNZ MAIN_L1
+            JMP EXIT_clicked
+
+            SPMP_clicked:
+                CMP isSP, 1
+                JZ SP_TOGGLE
+                    printGraphicalString MP_MSG, 0F1h, 34, 7
+                    printGraphicalString SP_MSG, 0F1h, 34, 7
+                    MOV isSP, 1
+                    JMP SPMP_end
+
+                SP_TOGGLE:
+                    printGraphicalString SP_MSG, 0F1h, 34, 7
+                    printGraphicalString MP_MSG, 0F1h, 34, 7
+                    MOV isSP, 0
+
+                SPMP_end:
+                JMP MAIN_L1
+            
+            MAKLA_clicked:
+                CMP isMaklaSif, 1
+                JZ MAKLA_TOGGLE
+                    printGraphicalString MKLASIF_MSG , 0F2h, 35, 12
+                    printGraphicalString MKLANSIF_MSG, 0F2h, 35, 12
+                    MOV isMaklaSif, 1
+                    JMP MAKLA_end
+
+                MAKLA_TOGGLE:
+                    printGraphicalString MKLANSIF_MSG, 0F2h, 35, 12
+                    printGraphicalString MKLASIF_MSG , 0F2h, 35, 12
+                    MOV isMaklaSif, 0
+
+                MAKLA_end:
+                JMP MAIN_L1
+
+            START_clicked:
+                ; START_GAME
+                JMP EXIT_clicked
+                JMP MAIN_L1
+        
+            EXIT_clicked:
 
         POP BP
         MOV AX, 4C00h
