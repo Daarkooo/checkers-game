52039936abbade14b7fa3e8a4e935d5f5848fa96
Medjber Abderrahim <medjber.abderrahim@gmail.com>
2024-05-02 11:57:42 +0100

    Successfully integrated menu/GUI with basic logic

--- src/main/main.asm
+++ src/main/main.asm
@@ -3,18 +3,35 @@
 .STACK 100h
 
 .DATA
-    myBoard         DB  20 DUP('b'), 10 DUP('e'), 20 DUP('w')
+    board           DB  20 DUP('b'), 10 DUP('0'), 20 DUP('w')
 
-    ; MEDJBER'S NEW VARIABLES, FOR CHOSING THEME WHEN GAME STARTS
+    ;? ****************************************************************
+    ;? MEDJBER'S NEW VARIABLES, FOR CHOSING THEME WHEN GAME STARTS
     blackCell       DW      0006h
     whiteCell       DW      000Fh
     blackPiece      DW      0000h
     whitePiece      DW      000Fh
+    ;? ****************************************************************
 
+    ;? ****************************************************************
+    ;? Hichem's logic variables
+    PColor          DW      ?
+    source_pawn     DB      ?
+    turn            DB      'b'
+    path1           DB      ?
+    path2           DB      ?
+    num             DB      ?
+    makla           DB      ?
+    isDirect        DB      ?
+    multiple_jumps  DB      ?
+    ;? ****************************************************************
+
+    INCLUDE menu.inc
     INCLUDE print.inc
-    INCLUDE menu.inc        ; ALREADY INCLUDES 'mouse.inc' AND 'GUI.inc'
+    INCLUDE logic.inc
 
 .CODE
+    
     main PROC
         MOV AX, @DATA
         MOV DS, AX
@@ -36,27 +53,74 @@
         JMP main_endLabel
         startClicked:
 
-            ; Clear screen by re-setting video mode
-            MOV AX, 0010h   ; 640x350 16 colors
-            INT 10h
+        ; Clear screen by re-setting video mode
+        MOV AX, 0010h   ; 640x350 16 colors
+        INT 10h
 
-            ; display mouse
-            MOV AX, 0001h
-            INT 33h
+        Board_init_GUI myBoard, blackCell, whiteCell, blackPiece, whitePiece
 
-            setMousePosition 0, 0
+        ; re-settingup mouse
+        setupMouse 500, 270, 0, 0, 637, 347
 
-            Board_init_GUI myBoard, blackCell, whiteCell, blackPiece, whitePiece
-            drawBorder 0008h, offsetX, offsetY, 340, 5
-            markCell 0004h, offsetX, offsetY, 34, 21
-            markCell 0004h, offsetX, offsetY, 34, 26
-            markCell 0007h, offsetX, offsetY, 34, 22
-            markCell 0007h, offsetX, offsetY, 34, 27
+        play:
+            reselect:
+                LEA AX,getCoordsFromMouseClick
+                awaitMouseClick AX, offsetX, offsetY, cellSize ; CX <- y DX <- x
+                
+                show_path board,DL,CL,turn,path1,path2,source_pawn,makla,isDirect,multiple_jumps
 
-        main_endLabel:
+            CMP path1,-1
+            JE label1
+                MOV AL,1
+            label1:
 
-        POP BP
-        MOV AX, 4C00h
-        INT 21h
-    main ENDP
+            CMP path2,-1
+            JE label2
+                MOV AL,1
+            label2:
+
+            CMP AL,1    
+            JE next
+                JMP reselect
+            next:
+
+            ; Use BX to pass 8 bit paraemtre, because AX will be cleared inside MACRO call
+            XOR BX, BX
+            MOV BL, path1
+            markCell 0Ah, offsetX, offsetY, cellSize, BX
+            
+            XOR BX, BX
+            MOV BL, path2
+            markCell 0Ah, offsetX, offsetY, cellSize, BX
+            
+            reselect2:
+            LEA AX,getCoordsFromMouseClick
+            awaitMouseClick AX, offsetY, offsetX, cellSize ; CX <- x DX <- y
+
+            move_pawn board,DL,CL,path1,path2,source_pawn,makla,isDirect
+
+            CMP isDirect,-1
+            JNE label3
+                JMP reselect2
+            label3:
+
+            XOR BX, BX
+            MOV BL, path1
+            markCell 06h, offsetX, offsetY, cellSize, BX
+
+            XOR BX, BX
+            MOV BL, path2
+            markCell 06h, offsetX, offsetY, cellSize, BX
+            
+			switch_turn turn ; make it here to change the color of the pawns (depends on player's turn)
+			
+			Move_GUI source_pawn,isDirect,PColor ; isDirect <- board[x,y] if the move is valid
+		JMP play
+		
+		main_endLabel:
+		
+		POP BP
+		MOV AX, 4C00h
+		INT 21h
+	main ENDP
 END main
