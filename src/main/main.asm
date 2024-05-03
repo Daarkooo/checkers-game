5d0c575a29c00a2c5f025f88da76087748c92f2a
Medjber Abderrahim <medjber.abderrahim@gmail.com>
2024-05-03 16:36:37 +0100

    Example game, has no ending

--- src/main/main.asm
+++ src/main/main.asm
@@ -22,6 +22,7 @@
     path2           DB      ?
     num             DB      ?
     makla           DB      ?
+    makla2          DB      ?
     isDirect        DB      ?
     multiple_jumps  DB      ?
     ;? ****************************************************************
@@ -31,7 +32,6 @@
     INCLUDE logic.inc
 
 .CODE
-    
     main PROC
         MOV AX, @DATA
         MOV DS, AX
@@ -57,16 +57,17 @@
         MOV AX, 0010h   ; 640x350 16 colors
         INT 10h
 
-        Board_init_GUI myBoard, blackCell, whiteCell, blackPiece, whitePiece
-
         ; re-settingup mouse
+        Board_init_GUI myBoard, blackCell, whiteCell, blackPiece, whitePiece
+        drawBorder 0008h, offsetX, offsetY 340, 5
         setupMouse 500, 270, 0, 0, 637, 347
 
         play:
+            PUSH CX
             reselect:
                 LEA AX,getCoordsFromMouseClick
                 awaitMouseClick AX, offsetX, offsetY, cellSize ; CX <- y DX <- x
-                
+
                 show_path board,DL,CL,turn,path1,path2,source_pawn,makla,isDirect,multiple_jumps
 
             CMP path1,-1
@@ -79,25 +80,25 @@
                 MOV AL,1
             label2:
 
-            CMP AL,1    
+            CMP AL,1
             JE next
                 JMP reselect
             next:
 
-            ; Use BX to pass 8 bit paraemtre, because AX will be cleared inside MACRO call
+            ; Use BX to pass paraemtre, because AX will be cleared inside MACRO call
             XOR BX, BX
             MOV BL, path1
             markCell 0Ah, offsetX, offsetY, cellSize, BX
-            
+
             XOR BX, BX
             MOV BL, path2
             markCell 0Ah, offsetX, offsetY, cellSize, BX
-            
+
             reselect2:
             LEA AX,getCoordsFromMouseClick
-            awaitMouseClick AX, offsetY, offsetX, cellSize ; CX <- x DX <- y
+            awaitMouseClick AX, offsetX, offsetY, cellSize ; CX <- x DX <- y
 
-            move_pawn board,DL,CL,path1,path2,source_pawn,makla,isDirect
+            move_pawn board, DL, CL, path1, path2, source_pawn, makla, makla2, isDirect
 
             CMP isDirect,-1
             JNE label3
@@ -106,21 +107,25 @@
 
             XOR BX, BX
             MOV BL, path1
-            markCell 06h, offsetX, offsetY, cellSize, BX
+            markCell blackCell, offsetX, offsetY, cellSize, BX
 
             XOR BX, BX
             MOV BL, path2
-            markCell 06h, offsetX, offsetY, cellSize, BX
-            
-			switch_turn turn ; make it here to change the color of the pawns (depends on player's turn)
-			
-			Move_GUI source_pawn,isDirect,PColor ; isDirect <- board[x,y] if the move is valid
-		JMP play
-		
-		main_endLabel:
-		
-		POP BP
-		MOV AX, 4C00h
-		INT 21h
-	main ENDP
+            markCell blackCell, offsetX, offsetY, cellSize, BX
+
+            switch_turn turn ; make it here to change the color of the pawns (depends on player's turn)
+
+            Move_GUI source_pawn, isDirect, PColor ; isDirect <- board[x,y] if the move is valid
+
+            POP CX
+            DEC CX
+            JCXZ main_endLabel
+        JMP play
+
+        main_endLabel:
+
+        POP BP
+        MOV AX, 4C00h
+        INT 21h
+    main ENDP
 END main
